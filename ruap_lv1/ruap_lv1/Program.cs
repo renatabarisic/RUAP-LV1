dc6e92daabe05ea71df835f7b6ac1007d67d03fa
renatabarisic <rbarisic@etfos.hr>
2022-10-26 15:40:33 +0200

    modified console app

---
+++ ruap_lv1/ruap_lv1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ruap_lv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Renate Barišić");
+        }
+    }
+}
