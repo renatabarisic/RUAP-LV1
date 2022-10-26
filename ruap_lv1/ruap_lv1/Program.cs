bb91a7e898564063354d5453673fcac55444014c
renatabarisic <rbarisic@etfos.hr>
2022-10-26 15:40:33 +0200

    after cherry picking

--- ruap_lv1/ruap_lv1/Program.cs
+++ ruap_lv1/ruap_lv1/Program.cs
@@ -6,8 +6,7 @@ namespace ruap_lv1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozdrav iz aplikacije od Renate Barišić, Broj indeksa: 0165081305");
-            
+            Console.WriteLine("Pozdrav iz aplikacije od Renate Barišić");
         }
     }
 }
